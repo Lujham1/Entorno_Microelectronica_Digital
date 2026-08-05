69468fdbbc87c6313b2ee7debbbabdadb99fe5d6
Lujham1 <141670424+Lujham1@users.noreply.github.com>
2026-08-05 00:34:11 -0300

    Change default WSL installation from Ubuntu 24.04 to 22.04

--- install-wsl.ps1
+++ install-wsl.ps1
@@ -3,7 +3,7 @@ Write-Host " Instalando WSL y Entorno VLSI para alumnos "
 Write-Host "=========================================="
 
 # Instalar WSL y Ubuntu 24.04 por defecto
-wsl --install -d Ubuntu-24.04
+wsl --install -d Ubuntu-22.04
 
 Write-Host "WSL instalado. Si es tu primera vez, Windows puede pedirte reiniciar."
 Write-Host "Una vez que Ubuntu esté listo y hayas creado tu usuario, copia y pega el comando de Linux en la terminal."
